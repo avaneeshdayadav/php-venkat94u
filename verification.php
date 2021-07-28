9e5511a57f38a7934e0dc64a9c4b70369b60dece
venkata eswara guptha <63256835+venkat94u@users.noreply.github.com>
2021-07-28 21:05:52 +0530

    verifying the email address

--- verification.php
+++ verification.php
@@ -20,7 +20,7 @@ if (isset($_GET['email']) && !empty($_GET['email']) && isset($_GET['hash']) && !
         if ($Var->affected_rows > 0) {
             echo "
                     <script>alert('Verification done successfully');
-                    window.location.href = 'http://" . $_SERVER['HTTP_HOST'] . "/assignment/index.php';
+                    window.location.href = 'http://" .$servername. "/assignment/index.php';
                     </script>
                 ";
         } else {
