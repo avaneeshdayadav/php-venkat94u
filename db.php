9f9f0da58aff7be820c15129ff3bbda1402d8a04
venkata eswara guptha <63256835+venkat94u@users.noreply.github.com>
2021-07-28 21:01:08 +0530

    This file is for database connection

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ $db_host = "host";//DataBase Connection
 $db_user = "user";
 $db_password = "password";
 $db_name = "database_name";
-$_SERVER['HTTP_HOST'] = "hostname";
+$servername = isset($_SERVER['HTTP_HOST'])?$_SERVER['HTTP_HOST']:' ';
 //Create Connection
 
 $conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
