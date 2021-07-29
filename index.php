5674c60b0c1e2566089a80318d65bd5c9eae5822
venkata eswara guptha <63256835+venkat94u@users.noreply.github.com>
2021-07-30 02:05:54 +0530

    used to take user input like username and password

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ if (isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['sname']) 
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>XKCD Challenge</title>
-    <link rel="stylesheet" href="xkcdstyles.css">
+    <link rel="stylesheet" href="css/xkcdstyles.css">
 </head>
 
 <body>
